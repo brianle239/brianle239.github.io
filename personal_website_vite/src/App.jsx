63f679b4fa547a5220999e7b327d671c22c95a3f
Brian Le <brianle239@gmail.com>
2024-01-10 23:49:57 -0800

    attempt to add functional components

--- personal_website_vite/src/App.jsx
+++ personal_website_vite/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 import Navbar from './components/navbar/navbar.jsx'
@@ -15,28 +14,6 @@ function App() {
 
   return (
     <div className='page1'>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h2>Greetings</h2>
-      <h1>Brian Le</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-      <Navbar />
       <Page1 />
       <Page2 />
       <Page3 />
@@ -45,5 +22,7 @@ function App() {
     
   )
 }
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(Navbar);
 
 export default App
--- personal_website_vite/src/components/navbar/navbar.jsx
+++ personal_website_vite/src/components/navbar/navbar.jsx
@@ -1,51 +1,65 @@
 import React, { Component } from "react";
+import ReactDOM from "react-dom/client";
+import { useEffect } from 'react';
+
 import './navbar.css'
 
-class Navbar extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render () {
-    return (
-        <nav className="navbar">
-         
-          <div className="navbarLogo">
-          <img src = "src/assets/koala_logo.png" className="koalaLogo">
-
-          </img>
-            
-            
-            <div className="nav_name">
-              Brian Le
-            </div>
-           
-          </div>
-          <div className="redirects">
-            <div>
-              Home
-            </div>
-            <div>
-              About
-            </div>
-            <div>
-              Technologies
-            </div>
-            <div>
-              Experience
-            </div>
-            <div>
-              Projects
-            </div>
-          </div>
-
+function Navbar() {
+ 
+  
+  useEffect(() => {
+    setCount(prevCount => prevCount + 1);
+  });
+  
+  
+//   return (
+//     <nav
+ 
+// className={isNavbarVisible ? 'visible' : 'hidden'}>
+//       </nav>
+//   );
 
   
-        </nav>
-    
-    );
+  return (
+      <nav className="navbar" onScroll={handleScroll}>
+        
+        <div className="navbarLogo">
+        <img src = "src/assets/koala_logo.png" className="koalaLogo">
 
-  }
+        </img>
+          
+          
+          <div className="nav_name">
+            Brian Le
+          </div>
+          
+        </div>
+        <div className="redirects">
+          <div>
+            Home
+          </div>
+          <div>
+            About
+          </div>
+          <div>
+            Technologies
+          </div>
+          <div>
+            Experience
+          </div>
+          <div>
+            Projects
+          </div>
+        </div>
+
+
+
+      </nav>
+  
+  );
+
 }
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(Navbar());
 
-export default Navbar;
+// export default Navbar;
