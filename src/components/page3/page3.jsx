ea977b74475fe98c5b8ee5079738053a596a174d
Brian Le <brianle239@gmail.com>
2024-02-07 23:32:29 -0800

    added experience

--- src/components/page3/page3.jsx
+++ src/components/page3/page3.jsx
@@ -13,11 +13,46 @@ class Page3 extends Component {
         <>
             
             <div className="page3">
-            <Wave_border_top />
-            <div className="border">
-            {/* <Wave_border />  */}
-            </div>
-            
+              <Wave_border_top />
+              <div className="border">
+              {/* <Wave_border />  */}
+              </div>
+
+              <div className="exp">
+                <div className="expTitle">
+                  Experience (Work in Progress)
+                </div>
+                <div className="expFlex">
+                  <div className="expCompName">
+                    Partner Engineering and Science, Inc
+                  </div>
+                  <div className="expPos">
+                    Backend Developer Intern,<br></br>
+                    Team Lead
+                  </div>
+                  <div className="expDesc">
+                    A sponsored internship experience where I got to lead a team of interns in developing
+                    a cutting edge AI model to generate reports and help digitilize information itno cloud databases
+                  </div>
+                </div>
+                <div className="expBorder" /> 
+              
+                <div className="expFlex">
+                  <div className="expCompName">
+                    Northrop Grumman
+                  </div>
+                  <div className="expPos">
+                    Software Engineer Intern
+                  </div>
+                  <div className="expDesc">
+                    Build upon a Data as a Service (Daas) project by 
+                    parsing and organizing terrabytes of GNSS data into a MySQL database for fast retrival time
+                    via a RESTful API. 
+                  </div>
+                </div>
+                <div className="expBorder" /> 
+              </div>
+              
             </div>
             
         </>
